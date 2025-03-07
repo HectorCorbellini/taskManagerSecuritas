a27907ad7215f79846a9a130a265382ff1dcfab6
Héctor Corbellini <hector.corbellini@gmail.com>
2025-03-07 02:07:45 -0300

    Extract database credentials to configuration file

--- src/main/java/com/securitas/util/DatabaseConnection.java
+++ src/main/java/com/securitas/util/DatabaseConnection.java
@@ -1,19 +1,35 @@
 package com.securitas.util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/library";
-    private static final String USER = "librarian";
-    private static final String PASSWORD = "placita";
-
+    private static final Properties properties = loadProperties();
     private static Connection connection;
+
+    private static Properties loadProperties() {
+        Properties props = new Properties();
+        try (InputStream input = DatabaseConnection.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                throw new RuntimeException("Unable to find application.properties");
+            }
+            props.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading application.properties", e);
+        }
+        return props;
+    }
 
     public static Connection getConnection() throws SQLException {
         if (connection == null || connection.isClosed()) {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            String url = properties.getProperty("db.url");
+            String user = properties.getProperty("db.user");
+            String password = properties.getProperty("db.password");
+            connection = DriverManager.getConnection(url, user, password);
         }
         return connection;
     }
